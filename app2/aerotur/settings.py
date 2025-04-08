ef88770237b88c30931fa64962e7d802ce0f46ad
Pedro Abilio Pereira Arcoverde <pedroarcoverde2@gmail.com>
2025-04-08 14:02:33 -0300

    funcionalidade do docker com o BD

--- app2/aerotur/settings.py
+++ app2/aerotur/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'aerotur.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME', 'crud_pedro'),
+        'USER': os.environ.get('DATABASE_USER', 'pedro'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'pedrosenha'),
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),  # Nome do serviço no docker-compose
+        'PORT': '5432',
     }
 }
 
