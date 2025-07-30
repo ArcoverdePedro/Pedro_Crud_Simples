379076c3a0e908c43cc03a6cd46fb7210a8a5df9
Pedro Abilio Pereira Arcoverde <pedroarcoverde2@gmail.com>
2025-07-30 16:32:27 -0300

    retirada do flowbite
    sem uso de flowbite

--- app_crud/views.py
+++ app_crud/views.py
@@ -1,7 +1,6 @@
-from multiprocessing import context
-from venv import create
 from .models import Pessoa
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 
 
 # Create your views here.
@@ -15,26 +14,54 @@ def home(request):
             pessoa = get_object_or_404(Pessoa, id=id_pessoa)
             pessoa.delete()
             return redirect("home")
+
         if "editar" in request.POST:
             id_pessoa = request.POST.get("id_pessoa")
             pessoa = get_object_or_404(Pessoa, id=id_pessoa)
-            pessoa.nome = request.POST.get("nome")
-            pessoa.idade = int(request.POST.get("idade"))
-            pessoa.cpf = request.POST.get("cpf")
+            nome = request.POST.get("nome")
+            try:
+                idade = int(request.POST.get("idade"))
+            except:
+                messages.error(request, "Use um número para idade")
+                return redirect("home")
+            cpf = request.POST.get("cpf").replace(".", "").replace("-", "")
+
+            pessoa.nome = nome if nome else pessoa.nome
+            pessoa.idade = idade if idade else pessoa.idade
+            pessoa.cpf = cpf if cpf else pessoa.cpf
+
             pessoa.save()
             return redirect("home")
+
         if "add_pessoa" in request.POST:
+            nome = request.POST.get("nome")
+            try:
+                idade = int(request.POST.get("idade"))
+            except:
+                messages.error(request, "Use um número para idade")
+                return redirect("home")
+            cpf = request.POST.get("cpf").replace(".", "").replace("-", "")
+
+            campo_falta = []
+
+            campo_falta.append("Nome da Pessoa") if not nome else None
+            campo_falta.append("Idade") if not idade else None
+            campo_falta.append("CPF") if not cpf else None
+
+            if campo_falta:
+                mensagem = "Estão faltando os seguintes campos: " + ", ".join(
+                    campo_falta
+                )
+                messages.error(request, mensagem)
+                return redirect("home")
+
             pessoa = Pessoa.objects.create(
-                nome=request.POST.get("nome"),
-                idade=int(request.POST.get("idade")),
-                cpf=request.POST.get("cpf"),
+                nome=nome,
+                idade=idade,
+                cpf=cpf,
             )
             return redirect("home")
 
     context["pessoas"] = pessoas
 
-    return render(
-        request,
-        "1_app_crud/home/home.html",
-        context
-    )
+    return render(request, "1_app_crud/home/home.html", context)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pedro-crud-simples!")
+
+
+if __name__ == "__main__":
+    main()
